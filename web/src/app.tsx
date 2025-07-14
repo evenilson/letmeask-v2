ffb05087aff934a62e3b1055507f23063c447580
Evenilson Liandro <evenilsonlp@gmail.com>
2025-07-14 09:29:53 -0300

    feat: implement question creation and audio recording features

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom, Room } from '@/pages'
+import { RecordRoomAudio } from './pages/record-room-audio'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,7 @@ export function App() {
         <Routes>
           <Route element={<CreateRoom />} index />
           <Route element={<Room />} path="room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
